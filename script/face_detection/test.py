f514e76c7fb65aa092a3435977b2e6c39555c29f
nenecchiCV <cafdr019@gmail.com>
2022-07-26 01:14:16 +0900

    bug fix

--- script/face_detection/test.py
+++ script/face_detection/test.py
@@ -24,11 +24,12 @@ def main():
         image_raw = np_sock.recieve()
         time_loc.text(str(now(start)))
         cropped_faces, bboxes = face_detector.crop_faces(image_raw)
-        detect_img = cv2.drawContours(image_raw, bboxes, -1, (0, 255, 0), thickness=2)
-        detect_image_rgb = Image.fromarray(cv2.cvtColor(detect_img, cv2.COLOR_BGR2RGB))
-        crop_image_rgb = Image.fromarray(cv2.cvtColor(cropped_faces[0], cv2.COLOR_BGR2RGB))
-        detect_image_loc.image(detect_image_rgb)
-        crop_image_loc.image(crop_image_rgb)
+        if len(cropped_faces) > 0:
+            detect_img = cv2.drawContours(image_raw, bboxes, -1, (0, 255, 0), thickness=2)
+            detect_image_rgb = Image.fromarray(cv2.cvtColor(detect_img, cv2.COLOR_BGR2RGB))
+            crop_image_rgb = Image.fromarray(cv2.cvtColor(cropped_faces[0], cv2.COLOR_BGR2RGB))
+            detect_image_loc.image(detect_image_rgb)
+            crop_image_loc.image(crop_image_rgb)
 
         if cv2.waitKey() & 0xFF == ord("q"):
             break
