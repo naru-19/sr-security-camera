0ef7ad3a898603dccca967b78fbfb25ac8fc43cf
nenecchiCV <cafdr019@gmail.com>
2022-07-25 11:40:52 +0900

    微修正

--- script/main.py
+++ script/main.py
@@ -3,23 +3,23 @@
 import streamlit as st
 from numpysocket import NumpySocket
 import cv2
-import time
+from narutils import *
 import numpy as np
 from PIL import Image
 
 
 def main():
     print("start")
-    start = time.time()
     st.markdown("# Camera")
     np_sock = NumpySocket()
     np_sock.startServer(41234)
+    start = now()
     time_loc = st.empty()
     image_loc = st.empty()
 
     while True:
         image_raw = np_sock.recieve()
-        time_loc.text(str(time.time() - start))
+        time_loc.text(str(now(start)))
         image_rgb = Image.fromarray(cv2.cvtColor(image_raw, cv2.COLOR_BGR2RGB))
         image_loc.image(image_rgb)
 
